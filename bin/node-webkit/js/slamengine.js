1e6d269c59ac53098dc44f6b8a4030d2a7fdc776
joneshf <jones3.hardy@gmail.com>
2014-07-03 18:41:23 -0700

    Restructure for node-webkit.
    Move normal bin to bin/browser.
    Added bin/node-webkit.
    Added task for building both browser and node-webkit.
    Added task for building a node-webkit distribution.

---
+++ bin/node-webkit/js/slamengine.js
@@ -0,0 +1,17 @@
+var spawn = require('child_process').spawn
+  , se = spawn('java', ['-jar', 'jar/slamengine_2.10-0.1-SNAPSHOT-one-jar.jar', '/home/joneshf/.config/slamdata/config.json'])
+  , gui = require('nw.gui')
+  ;
+
+gui.Window.get().on('close', function() {
+    se.kill();
+    this.close(true);
+});
+
+se.stdout.on('data', function (data) {
+    console.log('stdout: ' + data);
+});
+
+se.stderr.on('data', function (data) {
+    console.log('stderr: ' + data);
+});
--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp')
+  , spawn = require('child_process').spawn
   , clean = require('gulp-clean')
   , concat = require('gulp-concat')
   , nwBuilder = require('node-webkit-builder')
@@ -18,11 +19,21 @@ paths = {
     css: 'css',
     imgs: 'imgs/*',
     build: {
-        css: 'bin/css',
-        fonts: 'bin/fonts',
-        index: 'bin',
-        imgs: 'bin/imgs',
-        js: 'bin/js'
+        browser: {
+            css: 'bin/browser/css',
+            fonts: 'bin/browser/fonts',
+            imgs: 'bin/browser/imgs',
+            index: 'bin/browser',
+            js: 'bin/browser/js'
+        },
+        'node-webkit': {
+            css: 'bin/node-webkit/css',
+            fonts: 'bin/node-webkit/fonts',
+            imgs: 'bin/node-webkit/imgs',
+            index: 'bin/node-webkit',
+            jar: 'bin/node-webkit/jar',
+            js: 'bin/node-webkit/js'
+        }
     },
     concat: {
         js: [
@@ -105,47 +116,86 @@ gulp.task('watch', function() {
 });
 
 
-gulp.task('concat-js', function() {
+var concatJs = function(target) {
     return gulp.src(paths.concat.js)
       .pipe(concat(options.build.js))
-      .pipe(gulp.dest(paths.build.js));
-});
+      .pipe(gulp.dest(paths.build[target].js));
+};
 
-gulp.task('concat-css', function(){
+var concatCss = function(target) {
     var fa = gulp.src(paths.concat.css);
     var styles = gulp.src(paths.style)
         .pipe(sass());
 
     return es.concat(fa, styles)
         .pipe(concat(options.build.css))
-        .pipe(gulp.dest(paths.build.css));
-});
+        .pipe(gulp.dest(paths.build[target].css));
+};
 
-gulp.task('fonts', function() {
+var fonts = function(target) {
     return gulp.src(paths.concat.fonts)
-      .pipe(gulp.dest(paths.build.fonts));
-});
+      .pipe(gulp.dest(paths.build[target].fonts));
+};
 
-gulp.task('entypo', function() {
+var entypo = function(target) {
     return gulp.src(paths.concat.entypo)
-      .pipe(gulp.dest(paths.build.css));
+      .pipe(gulp.dest(paths.build[target].css));
+};
+
+var imgs = function(target) {
+    return gulp.src(paths.imgs)
+      .pipe(gulp.dest(paths.build[target].imgs));
+};
+
+gulp.task('slamengine-jar', function() {
+    return gulp.src('../slamengine/target/scala-2.10/slamengine_2.10-0.1-SNAPSHOT-one-jar.jar')
+        .pipe(gulp.dest(paths.build['node-webkit'].jar));
 });
 
-gulp.task('imgs', function() {
-    return gulp.src(paths.imgs)
-      .pipe(gulp.dest(paths.build.imgs));
+gulp.task('slamengine-js', function() {
+    return gulp.src('lib/node-webkit/js/slamengine.js')
+        .pipe(gulp.dest(paths.build['node-webkit'].js));
 });
 
 gulp.task('test', ['build'], function() {
     // Placeholder for test task.
 });
 
-gulp.task('build', ['compile', 'concat', 'fonts', 'entypo', 'imgs']);
-gulp.task('concat', ['concat-js', 'concat-css']);
+gulp.task('concat-js-browser', function() {return concatJs('browser');});
+gulp.task('concat-css-browser', function() {return concatCss('browser');});
+gulp.task('fonts-browser', function() {return fonts('browser');});
+gulp.task('entypo-browser', function() {return entypo('browser');});
+gulp.task('imgs-browser', function() {return imgs('browser');});
+
+gulp.task('concat-js-node-webkit', function() {return concatJs('node-webkit');});
+gulp.task('concat-css-node-webkit', function() {return concatCss('node-webkit');});
+gulp.task('fonts-node-webkit', function() {return fonts('node-webkit');});
+gulp.task('entypo-node-webkit', function() {return entypo('node-webkit');});
+gulp.task('imgs-node-webkit', function() {return imgs('node-webkit');});
+
+gulp.task('build-browser', [
+    'compile',
+    'concat-js-browser',
+    'concat-css-browser',
+    'fonts-browser',
+    'entypo-browser',
+    'imgs-browser'
+]);
+gulp.task('build-node-webkit', [
+    'compile',
+    'concat-js-node-webkit',
+    'concat-css-node-webkit',
+    'fonts-node-webkit',
+    'entypo-node-webkit',
+    'imgs-node-webkit',
+    'slamengine-jar',
+    'slamengine-js'
+]);
+gulp.task('build', ['build-browser', 'build-node-webkit']);
 gulp.task('default', ['compile', 'sass']);
 gulp.task('dist', ['build'], function() {
     nw = new nwBuilder({
-        files: 'bin/**',
+        files: 'bin/node-webkit/**',
         platforms: ['linux64', 'osx', 'win'],
         buildDir: 'dist'
     });
