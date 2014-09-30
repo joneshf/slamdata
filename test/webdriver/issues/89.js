b57f82ec314472385c3b417ae39726c35ba118d8
joneshf <jones3.hardy@gmail.com>
2014-09-30 09:11:27 -0700

    Squashed commit of the following:
    commit f46a7caf90c5dbec94a50f6d0c6cf65872af4fa5
    Author: joneshf <jones3.hardy@gmail.com>
    Date:   Mon Sep 29 19:59:34 2014 -0700
    
        Updated tests.
    
        Added one to check for a block in the same notebook.
        Also made the queries a bit more complicated (so it will create a new collection).
    
    commit 6caf944d45f7cbf292fabd3cd906963e79e7724f
    Author: joneshf <jones3.hardy@gmail.com>
    Date:   Mon Sep 29 19:13:34 2014 -0700
    
        Added some more tests.

--- test/webdriver/issues/89.js
+++ test/webdriver/issues/89.js
@@ -21,6 +21,8 @@ var assert = require('assert')
   , createBlock = '#notebook .content.active .create-block-button'
   , createSQL = createBlock + ' [title="SQL"]'
   , blockSelector = '#notebook .tabs-content .content.active .actual-content .block'
+  , editorSQL = blockSelector + ' textarea.block-editor'
+  , evaledSQL = blockSelector + ' .block-SQL .evaled-block span'
   ;
 
 test.describe('Queries executed relative to notebooks', function() {
@@ -35,20 +37,81 @@ test.describe('Queries executed relative to notebooks', function() {
             driver.findElement({css: '#add-notebook'}).click();
             driver.findElement({css: createBlock}).click();
             driver.findElement({css: createSQL}).click();
-            driver.findElement({css: blockSelector + ' textarea.block-editor'})
-                .sendKeys('select * from "../system/indexes"');
-            // Blur.
-            driver.findElement({css: createBlock}).click();
-            driver.findElement({css: createBlock}).click();
         });
     });
 
-    test.it('there should be something in there', function() {
+    test.it('`select * from "/system/indexes" limit 5`', function() {
+        driver.findElement({css: editorSQL})
+            .sendKeys('select * from "/system/indexes" limit 5');
+        // Blur.
+        driver.findElement({css: createBlock}).click();
+        driver.findElement({css: createBlock}).click();
+    });
+
+    test.it('should give back some results', function() {
         driver.sleep(15000).then(function() {
-            return driver.findElement({css: blockSelector + ' .block-SQL .evaled-block span'});
+            return driver.findElement({css: evaledSQL});
         }).then(function(el) {
             return el.getText();
         }).then(lambda.compose(assert)(op.notEqual('')));
+    });
+
+    // TODO: Reenable this after SE supports relative paths.
+    // test.it('`select * from "../system/indexes" limit 5`', function() {
+    //     driver.findElement({css: evaledSQL}).click();
+    //     driver.findElement({css: editorSQL}).clear();
+    //     driver.findElement({css: editorSQL})
+    //         .sendKeys('select * from "../system/indexes" limit 5');
+    //     // Blur.
+    //     driver.findElement({css: createBlock}).click();
+    //     driver.findElement({css: createBlock}).click();
+    // });
+
+    // test.it('should give back some results', function() {
+    //     driver.sleep(15000).then(function() {
+    //         return driver.findElement({css: evaledSQL});
+    //     }).then(function(el) {
+    //         return el.getText();
+    //     }).then(lambda.compose(assert)(op.notEqual('')));
+    // });
+
+    test.it('adding another SQL block', function() {
+        driver.findElement({css: createBlock}).click();
+        driver.findElement({css: createSQL}).click();
+    });
+
+    test.it('`select * from out0 limit 5`', function() {
+        driver.findElement({css: editorSQL})
+            .sendKeys('select * from out0 limit 5');
+        // Blur.
+        driver.findElement({css: createBlock}).click();
+        driver.findElement({css: createBlock}).click();
+    });
+
+    test.it('should give back some results', function() {
+        driver.sleep(15000).then(function() {
+            return driver.findElement({css: evaledSQL});
+        }).then(function(el) {
+            return el.getText();
+        }).then(lambda.compose(assert)(op.notEqual('')));
+    });
+
+    test.it('`select * from "system/indexes" limit 5`', function() {
+        driver.findElement({css: evaledSQL}).click();
+        driver.findElement({css: editorSQL}).clear();
+        driver.findElement({css: editorSQL})
+            .sendKeys('select * from "system/indexes" limit 5');
+        // Blur.
+        driver.findElement({css: createBlock}).click();
+        driver.findElement({css: createBlock}).click();
+    });
+
+    test.it('should give back no results', function() {
+        driver.sleep(15000).then(function() {
+            return driver.findElement({css: evaledSQL});
+        }).then(function(el) {
+            return el.getText();
+        }).then(lambda.compose(assert)(op.equal('')));
     });
 
     test.after(function() {
